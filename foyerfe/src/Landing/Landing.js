1f68afd34cc1432527fe940dd8404edcc153e798
jjblues86 <jeromejoof@yahoo.com>
2019-06-11 00:24:23 -0700

    created a username and password field using material ui

---
+++ foyerfe/src/Landing/Landing.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import RaisedButton from 'material-ui/RaisedButton';
+import TextField from 'material-ui/TextField';
+
+export default class Landing extends Component {
+    constructor(props){
+        super(props);
+        this.state={
+            username:'',
+            password:''
+        }
+    }
+    render() {
+        return (
+            <div>
+                <MuiThemeProvider>
+                    <TextField
+                        hintText="Please enter your Username"
+                        floatingLabelText="Username"
+                        onChange={(event, newValue) =>
+                            this.setState({username:newValue})}
+                        />
+                        <br/>
+                        <TextField
+                            hintText="Please enter your Password"
+                            floatingLabelText="Password"
+                            onChange={(event,newValue) =>
+                            this.setState({password:newValue})}
+                            />
+                            <br/>
+                            <RaisedButton label="Sign In" primary={true}/>
+                </MuiThemeProvider>
+            </div>
+        );
+
+    }
+
+};
