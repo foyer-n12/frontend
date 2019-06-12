b92dd5e8493c5b444cba68fea42635320ca62d63
jjblues86 <jeromejoof@yahoo.com>
2019-06-11 19:38:15 -0700

    login and resgister renders on page

--- foyerfe/src/App.js
+++ foyerfe/src/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import Landing from './Landing/Landing';
+import Register from './Landing/Register';
 
 export default class App extends React.Component {
     render(){
 
         return(
-            <Landing/>
+            <main>
+                <Landing/>
+                <br/>
+                <Register/>
+
+            </main>
+
+
         )
     }
 }
