3f9fc215ea985430bd5e3cb23bc3999c5a27e3a2
Alistair Blake <consultations@me.com>
2019-06-18 21:31:10 -0700

    clock is working

--- foyerfe/src/Dashboard/Dashboard.js
+++ foyerfe/src/Dashboard/Dashboard.js
@@ -3,6 +3,7 @@ import SearchBar from '../searchBar/SearchBar'
 import Favorites from '../favorites/Favorites'
 import NoteDashboard from "../Notes/NoteDashboard";
 import DarkSky from "../Weather/Weather";
+import Clock from "../Clock/Clock";
 
 export default class Dashboard extends Component {
     constructor(props){
@@ -18,6 +19,7 @@ export default class Dashboard extends Component {
                 <SearchBar/>
                 <Favorites/>
                 <NoteDashboard/>
+                <Clock/>
                 <DarkSky/>
             </div>
         )
