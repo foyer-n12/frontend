abf65da265298a5ab4597772acf4aff08e23c673
Connor Sihon <connorsihon@gmail.com>
2019-06-21 22:08:35 -0700

    what the fuck omg

--- foyerfe/src/App.js
+++ foyerfe/src/App.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import {BrowserRouter, Route} from "react-router-dom";
+<<<<<<< HEAD
 import AuthLanding from './Landing/Landing';
+=======
+import Landing from './Landing/Landing';
+import Dashboard from './Dashboard/Dashboard';
+>>>>>>> 9ef4769feac95031d1e165f1592079df23b7e623
 import AuthRedirect from './AuthRedirect/AuthRedirect';
 import {Dashboard} from "./Dashboard/Dashboard";
 
@@ -13,9 +18,9 @@ export default class App extends React.Component {
             <div>
                 <BrowserRouter>
                     <Route path="*" component={AuthRedirect}/>
-                    <Route exact path="/" component={AuthLanding}/>
-                    <Route exact path="/login" component={AuthLanding}/>
-                    <Route exact path="/signup" component={AuthLanding}/>
+                    {/*<Route exact path="/" component={Landing}/>*/}
+                    <Route exact path="/login" component={Landing}/>
+                    <Route exact path="/signup" component={Landing}/>
                     <Route exact path="/Dashboard" component={Dashboard}/>
                 </BrowserRouter>
             </div>
