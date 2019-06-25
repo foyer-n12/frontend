54905c3f09479ec5c2deb1b7b6056811d64f25d3
Karl Polintan <kf.polntan@gmail.com>
2019-06-24 20:12:51 -0700

    testing w/master

---
+++ foyerfe/src/__test__/Store.test.js
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+import Adapter from "enzyme-adapter-react-16";
+import {Provider} from 'react-redux';
+// import {mount} from 'enzyme';
+import Enzyme from 'enzyme';
+import configureStore from 'redux-mock-store';
+
+Enzyme.configure({adapter: new Adapter()});
+
+import App from '../App.js';
+
+const initialState = {
+    favorites: {favoritesArr : []},
+    soundcloud: {soundcloudLink: null},
+    note: null,
+};
+
+describe('This is store testing', () => {
+    it('Tests connection to the Store', () => {
+        const mockStore = configureStore([]);
+        const mountedApp = mount(<Provider store={mockStore(initialState)}><App/></Provider>);
+        expect(mountedApp.find('Dashboard')).toBeTruthy();
+    })
+});
